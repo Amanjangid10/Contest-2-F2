b4c4f584eb59b4a46efc2406b723c63f7be64f0f
Aman Jangid <98145943+Amanjangid10@users.noreply.github.com>
2023-03-18 23:23:35 +0530

    Contest solution

---
+++ index.js
@@ -0,0 +1,24 @@
+setTimeout(function openingceremoney() {
+    console.log("Let the game begin");
+    var score = new Object()
+    console.log(race100m, 3000)
+}, 1000);
+
+
+
+function race100m() {
+    var red = console.log(Math.floor(Math.random() * 5) + 10);
+    var blue = console.log(Math.floor(Math.random() * 5) + 10);
+    var green = console.log(Math.floor(Math.random() * 5) + 10);
+    var yellow = console.log(Math.floor(Math.random() * 5) + 10);
+
+    var leasttime = 0;
+    var secondleasttime = 0;
+
+    if (red < blue) {
+        leasttime = red;
+    } else {
+        leasttime = blue;
+    }
+
+}
